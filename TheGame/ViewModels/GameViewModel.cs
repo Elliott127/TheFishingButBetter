25056ef1451029ac3998ee87215c50c43c0f5e4d
Elliott <Elliott.Ortmann@chamonix.com.au>
2023-08-12 15:42:07 +0930

    Started implementing game logic

--- TheGame/ViewModels/GameViewModel.cs
+++ TheGame/ViewModels/GameViewModel.cs
@@ -1,12 +1,59 @@
-﻿using Game.ViewModels.Base;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Game.Services;
+using Game.ViewModels.Base;
+using TheGame.Views;
 
 namespace TheGame.ViewModels;
 
-public class GameViewModel : ViewModelBase
+public partial class GameViewModel : ViewModelBase
 {
+    private readonly IFileService fileService;
+    private readonly IPlayerService playerService;
+    private readonly List<Dictionary<string, string>> fishList;
+    private static int playersScore;
+
+    [ObservableProperty]
+    private string resultLabel = string.Empty;
+
+    [ObservableProperty]
+    private string score = playersScore.ToString();
+
+    public GameViewModel(IFileService fileService, IPlayerService playerService)
+    {
+        this.fileService = fileService;
+        this.playerService = playerService;
+        fishList = fileService.ReadFromCsv("Fishing.csv");
+        playersScore = 0;
+    }
 
     public override async Task InitialiseAsync(object navigationData)
     {
 
+    }
+
+    [RelayCommand]
+    public async Task OnCastLineClicked()
+    {
+        Random random = new();
+        int result = random.Next(1, 6);
+        var fishCaughtStats = fishList[result];
+        var fishCaughtName = fishCaughtStats["Name"];
+        ResultLabel = fishCaughtName;
+        bool answer = await App.Current.MainPage.DisplayAlert($"You Caught a {fishCaughtName}", "Would you like to keep it?", "Yes", "No");
+        string pointsToRetrieve = answer ? fishCaughtStats["Points if kept"] : fishCaughtStats["Points if released"];
+        if (fishCaughtStats["Fish Y/N"] == "Y")
+        {
+            playerService.AddCatch(fishCaughtName);
+        }
+
+        playersScore += Convert.ToInt32(pointsToRetrieve);
+        Score = playersScore.ToString();
+    }
+
+[RelayCommand]
+    public async Task EndGame()
+    {
+        await Shell.Current.GoToAsync(nameof(LoginView), true);
     }
 }
--- TheGame/Views/GameView.xaml.cs
+++ TheGame/Views/GameView.xaml.cs
@@ -1,39 +1,12 @@
+using TheGame.ViewModels;
+
 namespace TheGame.Views;
 
 public partial class GameView : ContentPage
 {
-	public GameView()
+	public GameView(GameViewModel viewModel)
 	{
+        this.BindingContext = viewModel;
 		InitializeComponent();
 	}
-	private void OnCastLineClicked(object sender, EventArgs e)
-{
-    Random random = new();
-    int result = random.Next(1, 7);
-
-    if (result == 1)
-    {
-        ResultLabel.Text = "You caught a small fish!";
-    }
-    else if (result == 2)
-    {
-        ResultLabel.Text = "You caught a medium fish!";
-    }
-    else if (result == 3)
-    {
-        ResultLabel.Text = "You caught a large fish!";
-    }
-    else if (result == 4)
-    {
-        ResultLabel.Text = "You caught a boot...";
-    }
-    else if (result == 5)
-    {
-        ResultLabel.Text = "You caught a tin can...";
-    }
-    else
-    {
-        ResultLabel.Text = "You caught nothing...";
-    }
-}
 }
